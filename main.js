68a89fab80062bbae5576175003e137e224d5ac9
Cannellespeculos <cjeanpierre@edenschool.fr>
2023-03-13 17:15:49 +0100

    tried to finish plats

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
 import "./style.css";
+const cartArt1 = document.querySelector("cartArt1");
+
+const Artcontent = [
+  {
+    imgSrc: "pic01.jpg",
+    title: "un super titre",
+    text: "lorem ipsum...",
+  },
+];
+
+for (let i = 0; i < Artcontent.length; i++) {
+  const a = Artcontent[i];
+  for (let index = 0; index < a.length; index++) {
+    console.log(a.imgSrc);
+  }
+}
+
+// cartArt1.innerHTML = `title = ${Artcontent.title}`;
 
